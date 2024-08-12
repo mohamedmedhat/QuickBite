315ea1505307197c423d63cbacf9e791d0e97976
moMedhat <mmr973320@gmail.com>
2024-08-12 15:19:41 +0300

    make app arc

---
+++ app/src/main/java/com/xapp/quickbit/data/source/remote/network/restapi/RetrofitModule.kt
@@ -0,0 +1,4 @@
+package com.xapp.quickbit.data.source.remote.network.restapi
+
+class RetrofitModule {
+}
---
+++ app/src/main/java/com/xapp/quickbit/presentation/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xapp.quickbit.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xapp.quickbit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
