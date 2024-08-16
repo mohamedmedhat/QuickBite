858db95eb032994c8de9eeab380efebd434704d9
moMedhat <mmr973320@gmail.com>
2024-08-16 06:01:33 +0300

    make most login ui

--- app/src/main/java/com/xapp/quickbit/presentation/fragment/LoginFragment.kt
+++ app/src/main/java/com/xapp/quickbit/presentation/fragment/LoginFragment.kt
@@ -1,20 +1,18 @@
 package com.xapp.quickbit.presentation.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.xapp.quickbit.databinding.FragmentLoginBinding
+import com.xapp.quickbit.presentation.activity.RecipeActivity
 
 class LoginFragment : Fragment() {
     private lateinit var _binding: FragmentLoginBinding
     private val binding get() = _binding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +22,22 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        try {
+            handleOnClicks()
+        } catch (error: IllegalArgumentException) {
+            throw error
+        }
+    }
+
+
+    private fun handleOnClicks() {
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(requireContext(), RecipeActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
 }
