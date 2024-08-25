75c0e821a78f450b49ec48e8a4cc126023b5cd25
moMedhat <mmr973320@gmail.com>
2024-08-25 19:39:52 +0300

    [add]: make delete favourite item

--- app/src/main/java/com/xapp/quickbit/viewModel/adapter/FavouriteAdapter.kt
+++ app/src/main/java/com/xapp/quickbit/viewModel/adapter/FavouriteAdapter.kt
@@ -10,15 +10,21 @@ import com.bumptech.glide.Glide
 import com.xapp.quickbit.R
 import com.xapp.quickbit.data.source.local.entity.MealInformationEntity
 
-class FavouriteAdapter(private val items: List<MealInformationEntity>, private val onItemClick: (MealInformationEntity) -> Unit) : RecyclerView.Adapter<FavouriteAdapter.FavouriteAdapterViewHolder>() {
+class FavouriteAdapter(
+    private val items: MutableList<MealInformationEntity>,
+    private val onItemClick: (MealInformationEntity) -> Unit,
+    private val onDeleteClick: (MealInformationEntity) -> Unit
+) : RecyclerView.Adapter<FavouriteAdapter.FavouriteAdapterViewHolder>() {
 
     class FavouriteAdapterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val cardFavImage: ImageView = view.findViewById(R.id.img_fav_meal)
         val cardFavText: TextView = view.findViewById(R.id.tv_fav_meal_name)
+        val cardUnSave: ImageView = view.findViewById(R.id.iv_saveIcon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavouriteAdapterViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_favourite_item, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_favourite_item, parent, false)
         return FavouriteAdapterViewHolder(view)
     }
 
@@ -28,11 +34,21 @@ class FavouriteAdapter(private val items: List<MealInformationEntity>, private v
             .load(item.mealThumb)
             .into(holder.cardFavImage)
         holder.cardFavText.text = item.mealName
-
         holder.itemView.setOnClickListener {
             onItemClick(item)
+        }
+        holder.cardUnSave.setOnClickListener {
+            onDeleteClick(item)
         }
     }
 
     override fun getItemCount(): Int = items.size
+
+    fun removeItem(meal: MealInformationEntity) {
+        val position = items.indexOf(meal)
+        if (position != -1) {
+            items.removeAt(position)
+            notifyItemRemoved(position)
+        }
+    }
 }
